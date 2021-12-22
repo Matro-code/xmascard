1c8e1e9dd512bb537e143b1ae4615d0a7aac5dec
Matro-code <95519603+Matro-code@users.noreply.github.com>
2021-12-22 06:14:40 +0100

    Add files via upload

---
+++ name.js
@@ -0,0 +1,98 @@
+const name = getNameFromUrl() || "Blockfertigung";
+
+const nameEle = document.getElementById("name");
+// new_1
+var alleZeichen="ABCDEFGHIJKLMNOPQRSTUVWXYZÜ.Ö-";
+const MA = decode();
+const outName = MA;
+
+// end_1
+// new_2
+var Ausgabe1;
+var Ausgabe2;
+var Text1="Frohe Weihnachten und bleibt gesund";
+var Text2="Frohe Weihnachten";
+// end_2
+
+
+createNameSpans(outName); //name
+
+const letterEles = document.querySelectorAll(".letter");
+
+console.log(letterEles);
+
+lightUpName(1);
+
+function createNameSpans(Name) { 
+  let nameArray = outName.split(""); // name.split("");
+  let htmlString = "";
+  nameArray.forEach((letter, index) => {
+    htmlString += `<span class="letter" id="letter-${index}">${letter}</span>`;
+  });
+  nameEle.innerHTML = htmlString;
+}
+
+function lightUpName(n) {
+  let length = letterEles.length;
+  for (let i = 0; i < length; i++) {
+    let ele = letterEles[i];
+    ele.className = (n + i) % 2 == 0 ? "green" : "red";
+    // ternary syntax: bedingung ? true : falsch;
+  }
+
+  setTimeout(function () {
+    lightUpName(n + 1);
+  }, 500);
+}
+
+function getNameFromUrl() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  let urlName = urlParams.get("name");
+  return urlName;
+}
+// new_1
+function ucFirst(string) {
+  return string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
+}
+function decode(){
+  var Vtext=name;//Vt.value.toUpperCase();
+  var Schluessel="GNUAHVOBIPWCJQXDKRYELSZFMTÜ.Ö-";//Schl.value;
+  var Decodiert="";
+  var Stelle;
+  for (i=0; i < Vtext.length ; i++ ) {
+    Stelle = Schluessel.indexOf(Vtext.charAt(i));
+    if (Stelle > -1) {
+      Decodiert = Decodiert + alleZeichen.charAt(Stelle);}
+  }
+    let outMA = ucFirst(Decodiert);
+alert(outMA);
+if (outMA==="Freiherr-vom-stein-gruppe") {
+	outMA="Freiherr-vom-Stein-Gruppe";
+}
+  return outMA;
+  }
+
+// Textauswahl
+      var datei = outName;
+      var outTXT = datei + ".html";
+      const embed = document.getElementById("MAtext");
+      embed.src=outTXT;
+
+// Grußauswahl
+            
+      const P = ["Tina", "Sylvus", "Anke.", "Freiherr-vom-Stein-Gruppe"];      
+             if(P.indexOf(outName) > -1) {    // return true
+	document.getElementById("bild").src="Bild2.jpg"; 
+	document.getElementById("Gruß").innerHTML = "Ela";
+//             } else {   // return false
+//	document.getElementById("bild").src="Bild2.jpg"; 
+//	document.getElementById("Gruß").innerHTML = "Ela";
+             }
+      
+           
+
+
+
+
+
